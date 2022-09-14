9048daca10ddb44f42d12b739f98275b263b642b
erick <emayorga30@gmail.com>
2022-09-14 09:41:58 -0600

    Update

---
+++ Utils/Variables.cpp
@@ -0,0 +1,9 @@
+//
+// Created by erick on 10/09/22.
+//
+#include "../Utils/Variables.h"
+
+std::vector<ParticionesMontadas> particiones_montadas;
+User _user_logged;
+
+int number_id;
---
+++ Utils/Variables.h
@@ -0,0 +1,15 @@
+//
+// Created by erick on 10/09/22.
+//
+
+#ifndef MIA_PROYECTO1_201901758_2S2022_VARIABLES_H
+#define MIA_PROYECTO1_201901758_2S2022_VARIABLES_H
+#include <vector>
+#include "Structures.h"
+#include "AdminStructure.h"
+
+extern std::vector<ParticionesMontadas> particiones_montadas;
+extern User _user_logged;
+extern int number_id;
+
+#endif //MIA_PROYECTO1_201901758_2S2022_VARIABLES_H
